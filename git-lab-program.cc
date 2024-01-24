79ab751017dec04a48f81c03ceec5108a85c5f29
Ayush Patel <ap366222@ohio.edu>
2024-01-24 18:20:42 -0500

    Commit 1

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ayush Patel
+ *        Date: 1/24/2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    return 0;
+    cout << "Hello Git!!" << endl;
+}// main
